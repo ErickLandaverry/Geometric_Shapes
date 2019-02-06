830257bfb11d91c0d61814b60c26a9c0a7993957
landy <landy.erick20@gmail.com>
2019-02-05 23:53:15 -0700

    Intial Commit. Created an image using the Turtle Module in Python.

---
+++ Geometric_Shapes.py
@@ -0,0 +1,19 @@
+import turtle
+import random
+
+mike = turtle.Turtle()
+mike.color('green')
+turtle.bgcolor('black')
+colors = ["purple" , "green" , "orange" , "cyan"]
+mike.speed(50)
+
+for i in range(10):
+    for i in range(2):
+        mike.forward(100)
+        mike.right(60)
+        mike.forward(100)
+        mike.right(120)
+    mike.right(36)
+    mike.color(random.choice(colors))
+
+exitonclick()
